afb9c27a374089f28f14e8322fa842a203a673a2
WahVin <ngurahkelvin27@gmail.com>
2023-10-10 14:58:46 +0800

    commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 //ES6 
-//import  Express  from "express";
 const Express = require("express");
-//import bodyparser from "body-parser"; 
 const bodyparser = require("body-parser");
 const user = require("./app/routes/user");
 const Blogs = require("./app/routes/blog");
@@ -13,15 +11,14 @@ const cors = require("cors")
 
 const corsOptions = {
     link: '*',
-    origin: 'http://127.0.0.1/',
-    methods: 'GET, PUT, POST, PATCH, DELETE'
+    // origin: 'http://127.0.0.1:5501/',
   };
 
  app.use(bodyparser.urlencoded({ extended: true }))
  app.use(bodyparser.json());
  app.use(cors(corsOptions));
 
-// routing 
+
 app.use("/User", user);
 app.use("/Blogs", Blogs);
 app.use("/Video", videos);
@@ -30,27 +27,3 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
- //app.use(Expresss.static("public"));
-
- 
-//  app.get("/videoEdu", (request, response) => {
-//     response.json({
-//         data: {
-//         Title: "Present Tense",
-//         author: "Michel",
-//         Deskription: " jadjjdjhhhh",
-//         Category: "SMP",  
-//         Durasi: 11,
-//         },
-//     })
-//  });
-
-// const server = http.createServer((req, res) => {});
-
-
-// app.post("/videoEdu", (request, response) =>  {
-//     console.log({request});
-//     response.json({
-//         author: request.body.author,
-//     });
-// })
